4939a435bb1cfd764ca47ea81879f139eb249408
AshleyMush <tanaka.mush@gmail.com>
2023-09-05 01:55:57 +0100

    Initial Commit: "Initial commit for Snake game." "Starting the Snake game project."
    Added Basic Functionality:
    "Implemented basic movement for the snake."
    "Added food spawning and snake growth."
    
    Bug Fixes:
    
    "Fixed bug where snake could move in opposite direction."
    "Resolved issue with food spawning inside snake."

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import random
-import smtplib
-
-import string
-
-def generate_otp(length):
-    characters = string.digits  # Use only digits
-    otp = ''
-    for _ in range(length):
-        otp += random.choice(characters)
-    return otp
-
-# Generate a 6-digit OTP
-otp = generate_otp(6)
-print("Generated OTP:", otp)
-
-
-
-my_email = "YOUR_EMAIL@Gmail.Com"
-password = "ABC123"
-
-receiver = "RECEIVINGEMAIL@GMAIL,COM"
-
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs=receiver,
-                        msg=f"SUBJECT:One-time-passcode (OTP)\n\nThe OTP for your account is {otp}. Enter it now to continue logging in.")
+# import random
+# import smtplib
+#
+# import string
+#
+# def generate_otp(length):
+#     characters = string.digits  # Use only digits
+#     otp = ''
+#     for _ in range(length):
+#         otp += random.choice(characters)
+#     return otp
+#
+# # Generate a 6-digit OTP
+# otp = generate_otp(6)
+# print("Generated OTP:", otp)
+#
+#
+#
+# my_email = "Opportunityhubzw@Gmail.Com"
+# password = "vmlvmqygizoyuesv"
+#
+# receiver = "Email2_Test@Yahoo.Com"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs=receiver,
+#                         msg=f"SUBJECT:One-time-passcode (OTP)\n\nThe OTP for your account is {otp}. Enter it now to continue logging in.")
